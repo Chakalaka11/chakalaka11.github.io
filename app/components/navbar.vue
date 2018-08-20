6d06268f136c68dc6bc6d60a280de8f7d83497da
Dyomin Victor <dyomin_vic@ukr.net>
2018-08-20 22:58:57 +0300

    initial

---
+++ app/components/navbar.vue
@@ -0,0 +1,24 @@
+
+<template>
+    <div class="nav-container">
+        <div>
+
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:"navbar",
+    data: function(){
+        return{
+
+        };
+    }
+};
+</script>
+<style>
+.nav-container
+{
+    width: 100%
+}
+</style>
